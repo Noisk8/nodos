c3a0839d4375faf1e45e1d5bf980d0f9f9e7d879
noisk8 <juansk8jaramillo@gmail.com>
2023-11-28 22:06:05 -0500

    dexente 1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,28 @@
 import React from "react";
-import {Image} from "@nextui-org/react";
-
+import { Image } from "@nextui-org/react";
 
 interface HeaderProps {
-    parrafo : string;
-    imageSrc: string;
-    imageAlt: string;
+  parrafo: string;
+  imageSrc: string;
+  imageAlt: string;
 }
 
-export default function Header ({parrafo, imageSrc, imageAlt}: HeaderProps){
+export default function Header({ parrafo, imageSrc, imageAlt }: HeaderProps) {
+  return (
+    <div className="flex justify-center items-center ">
+      <h1 className="animate-pulse text-center text-4xl font-semibold leading-8 text-rose-200 pt-20 pb-10 mb-12">
+        {parrafo}
+      </h1>
 
-return (
-<div className="flex justify-center items-center mt-16"> 
-
-<h1 className="animate-pulse text-center text-4xl font-semibold leading-8 text-rose-200 pt-20 pb-10 mb-12">{parrafo}</h1>
-
-<div className="flex flex-col items-center ml-8">
-  <Image
-    isZoomed
-    width={240}
-    alt={imageAlt}
-    className="animate-pulse object-cover rounded-xl opacity-50 "
-    src={imageSrc}
-  />
-
-</div>
-</div>
-)
+      <div className="flex flex-col items-center ml-8">
+        <Image
+          isZoomed
+          width={240}
+          alt={imageAlt}
+          className="animate-pulse object-cover rounded-xl opacity-50 "
+          src={imageSrc}
+        />
+      </div>
+    </div>
+  );
 }
