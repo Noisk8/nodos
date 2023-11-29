93edf5ed8dc415f1a6113c3e1f00bc9aedc8cc74
noisk8 <juansk8jaramillo@gmail.com>
2023-11-29 01:03:15 -0500

    responsive 0.1

--- src/app/components/Carreto.tsx
+++ src/app/components/Carreto.tsx
@@ -5,13 +5,20 @@ import {Link} from "@nextui-org/react";
 
 export default function Carreto() {
   return (
-<div className="bg-gray-900 p-6 mt-10 rounded-lg shadow-xl text-white mx-auto max-w-2xl mb-8" style={{ boxShadow: "0 8px 12px -2px rgba(255, 0, 255, 0.1), 0 4px 8px -2px rgba(255, 0, 255, 0.06)" }}>
-  <div className="flex justify-center items-center">
-    <div className="text-neon">
-      <p>🧑‍💻 Administramos dos nodos de nym los cuales son de <a href="#" className="text-fuchsia-500 hover:underline">DAOariwas</a>, destinamos un 10% de la operación en causas sociales en Latinoamérica.</p>
-      <br />
-      <p>🫂 Con el staking en nuestro nodo estás apoyando a personas de comunidades de las diferentes periferias de este hermoso continente</p>
-    </div>
+<div className="bg-gray-900 p-6 mt-10 rounded-lg shadow-xl text-white mx-auto max-w-2xl mb-8 md:mb-0" style={{ boxShadow: "0 8px 12px -2px rgba(255, 0, 255, 0.1), 0 4px 8px -2px rgba(255, 0, 255, 0.06)" }}>
+  <div className="text-neon">
+    <p className="md:text-lg">
+      🧑‍💻 Administramos dos nodos de nym los cuales son de{" "}
+      <a href="#" className="text-fuchsia-500 hover:underline">
+        DAOariwas
+      </a>
+      , destinamos un 10% de la operación en causas sociales en Latinoamérica.
+    </p>
+    <br />
+    <p className="md:text-lg">
+      🫂 Con el staking en nuestro nodo estás apoyando a personas de comunidades
+      de las diferentes periferias de este hermoso continente
+    </p>
   </div>
 </div>
   );
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,16 +10,16 @@ interface HeaderProps {
 export default function Header({ parrafo, imageSrc, imageAlt }: HeaderProps) {
   return (
     <div className="flex justify-center items-center flex-wrap ">
-      <h1 className="animate-pulse text-center text-4xl font-semibold leading-8 text-rose-200 pt-20 pb-10 mb-12">
+      <h1 className="animate-pulse text-center text-4xl font-semibold leading-8 text-rose-200 pt-4 pb-10 mb-2">
         {parrafo}
       </h1>
 
-      <div className="flex flex-col items-center ml-8 ">
+      <div className="flex flex-col items-center ">
         <Image
           isZoomed
           width={240}
           alt={imageAlt}
-          className="animate-pulse object-cover   "
+          className="animate-pulse object-cover"
           src={imageSrc}
         />
       </div>
