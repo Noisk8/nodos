9ea2774b29ab9ad71e562a453ba193dc46f57caf
Noisk8 <juansk8jaramillo@gmail.com>
2024-11-12 17:19:05 -0500

    responsive navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -10,44 +10,26 @@ import {
 } from "@nextui-org/react";
 
 export default function NavBar() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-
-  const menuItems = [
-    "Profile",
-    "Dashboard",
-    "Activity",
-    "Analytics",
-    "System",
-    "Deployments",
-    "My Settings",
-    "Team Settings",
-    "Help & Feedback",
-    "Log Out",
-  ];
+ 
 
   return (
 
+  <Navbar position="static" className="pt-8">
+    <Link className="text-4xl" href="/nodos" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> </Link>
 
-    <Navbar position="static" className="pt-8">
-   <Link className="text-4xl" href="/nodos" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> </Link >
-
-    <NavbarContent className="hidden sm:flex gap-4" justify="center">
+    <NavbarContent className="flex flex-col sm:flex-row gap-4" justify="center">
       <NavbarItem>
-      <Link className="text-4xl" href="/nodos" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> ⚗️ Nodos</Link >
-
+        <Link className="text-4xl" href="/nodos" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> ⚗️ Nodos</Link>
       </NavbarItem>
       <NavbarItem isActive>
-      <Link className="text-4xl" href="/gateways" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> ⚗️ Gateways</Link >
-
+        <Link className="text-4xl" href="/gateways" style={{ textShadow: "2px 2px 4px #FF00FF", color: "#FF00FF" }}> ⚗️ Gateways</Link>
       </NavbarItem>
-     
     </NavbarContent>
     <NavbarContent justify="end">
       <NavbarItem className="hidden lg:flex">
         <Link href="#"></Link>
       </NavbarItem>
       <NavbarItem>
-    
       </NavbarItem>
     </NavbarContent>
   </Navbar>
